25976674c75572f7fe80fee065161f47c28002ff
Łukasz Lepianka <76851989+Luki308@users.noreply.github.com>
2022-12-12 03:36:39 +0100

    Small changes in functions. Add pdf with report

--- Main.m
+++ Main.m
@@ -9,12 +9,13 @@ a = 0;
 b = 1;
 c = 0;
 d = 2*pi;
-n = 1000;
-m = 1000;
+n = 100;
+m = 100;
 
 % Liczenie numeryczne
 wynik_trapezy = trapezy(@fBiegunowe,a,b,c,d,n,m);
-blad = dokladna - wynik_trapezy;
+blad_bezwzgledny = wynik_trapezy - dokladna;
+blad_wzgledny = abs(wynik_trapezy - dokladna)/abs(dokladna);
 
 % Tworzenie siatki do wykresu
 r = linspace(a,b);
@@ -29,11 +30,12 @@ Z = f(X,Y);
 % Rysowanie wykresu
 figure
 mesh(X,Y,Z)
-title('f(x,y) = x^3-y^2')
+title('f(x,y) = $log(x^2+y^2+0.0001)$','Interpreter','latex','FontSize',22)
 xlabel('x');
 ylabel('y');
 zlabel('f(x,y)')
 annotation('textbox', [0.65, 0.85, 0.1, 0.1], 'String', "Dokładna całka =  " + dokladna)
 annotation('textbox', [0.65, 0.75, 0.1, 0.1], 'String', "Przybliżona całka = " + wynik_trapezy)
-annotation('textbox', [0.65, 0.65, 0.1, 0.1], 'String', "Błąd = " + blad)
+annotation('textbox', [0.65, 0.7, 0.1, 0.1], 'String', "Błąd wzgledny= " + blad_wzgledny)
+annotation('textbox', [0.65, 0.65, 0.1, 0.1], 'String', "Błąd bezwzgledny= " + blad_bezwzgledny)
 grid on
--- f.m
+++ f.m
@@ -2,7 +2,7 @@ function [wartosc] = f(x,y)
 % Definicja dowolnej funkcji f: R^2 -> R
 % we współrzędnych kartezjańskich
 
-% wartosc = 1 + 0*x + y*0;
-wartosc = x.^3-y.^2;
+wartosc = log(x.^2+y.^2+0.0001);
 end
+
 
